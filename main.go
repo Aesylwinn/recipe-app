226b015d3e90947072662e4091b959328dacad1e
Kyle Cooley <kyleacooley@gmail.com>
2019-03-30 16:08:22 -0400

    Simple app

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gorilla/mux"
+)
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/", TestHandler)
+  http.ListenAndServe(":8080", r)
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello World!\n"))
+}
+
